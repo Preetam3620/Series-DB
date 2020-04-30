f42bf45b7c75ca9f53eefdad46cccaa040c0ba92
Preetam3620 <preetamamaske1@gmail.com>
2020-04-30 21:57:22 +0530

    table complete, tv series append db

--- app.js
+++ app.js
@@ -19,51 +19,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 /* express session settings */
 app.use(
-    session({
-        secret: "A secret.",
-        resave: false,
-        saveUninitialized: false,
-    })
+	session({
+		secret: "A secret.",
+		resave: false,
+		saveUninitialized: false,
+	})
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.connect(
-    "mongodb+srv://" +
-        process.env.MONGO_USER +
-        ":" +
-        process.env.MONGO_PWD +
-        "@cluster0-bq71x.mongodb.net/seriesDB",
-    { useNewUrlParser: true, useUnifiedTopology: true }
+	"mongodb+srv://" +
+		process.env.MONGO_USER +
+		":" +
+		process.env.MONGO_PWD +
+		"@cluster0-bq71x.mongodb.net/seriesDB",
+	{ useNewUrlParser: true, useUnifiedTopology: true }
 );
 // mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set("useCreateIndex", true);
 
 /* mongoose model */
 const seriesSchema = new mongoose.Schema({
-    Title: String,
-    Year: String,
-    imdbRating: String,
-    totalSeasons: String,
-    Genre: String,
+	Title: String,
+	Year: String,
+	imdbRating: String,
+	totalSeasons: String,
+	Genre: String,
 });
 
 const moviesSchema = new mongoose.Schema({
-    Title: String,
-    Year: String,
-    Director: String,
-    imdbRating: String,
-    Genre: String,
+	Title: String,
+	Year: String,
+	Director: String,
+	imdbRating: String,
+	Genre: String,
 });
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    secret: String,
-    googleId: String,
-    series: [seriesSchema],
-    movies: [moviesSchema],
+	username: String,
+	password: String,
+	secret: String,
+	googleId: String,
+	series: [seriesSchema],
+	movies: [moviesSchema],
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -74,176 +74,166 @@ const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
 passport.serializeUser(function (user, done) {
-    done(null, user.id);
+	done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
-    User.findById(id, function (err, user) {
-        done(err, user);
-    });
+	User.findById(id, function (err, user) {
+		done(err, user);
+	});
 });
 
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            // callbackURL: "",
-            callbackURL: "http://localhost:3000/auth/google/secrets",
-            userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-        },
-        function (accessToken, refreshToken, profile, cb) {
-            User.findOrCreate(
-                { googleId: profile.id, username: profile.displayName },
-                function (err, user) {
-                    return cb(null, user);
-                }
-            );
-        }
-    )
+	new GoogleStrategy(
+		{
+			clientID: process.env.CLIENT_ID,
+			clientSecret: process.env.CLIENT_SECRET,
+			// callbackURL: "",
+			callbackURL: "http://localhost:3000/auth/google/secrets",
+			userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+		},
+		function (accessToken, refreshToken, profile, cb) {
+			User.findOrCreate({ googleId: profile.id, username: profile.displayName }, function (
+				err,
+				user
+			) {
+				return cb(null, user);
+			});
+		}
+	)
 );
 
 /* get routes */
 app.get("/", function (req, res) {
-    res.render("home");
+	res.render("home");
 });
 
-app.get(
-    "/auth/google",
-    passport.authenticate("google", { scope: ["profile"] })
-);
+app.get("/auth/google", passport.authenticate("google", { scope: ["profile"] }));
 
 app.get(
-    "/auth/google/secrets",
-    passport.authenticate("google", { failureRedirect: "/login" }),
-    function (req, res) {
-        // Successful authentication, redirect home.
-        res.redirect("/lists");
-    }
+	"/auth/google/secrets",
+	passport.authenticate("google", { failureRedirect: "/login" }),
+	function (req, res) {
+		// Successful authentication, redirect home.
+		res.redirect("/lists");
+	}
 );
 
 app.get("/login", function (req, res) {
-    res.render("login");
+	res.render("login");
 });
 
 app.get("/register", function (req, res) {
-    res.render("register");
+	res.render("register");
 });
 
 app.get("/logout", function (req, res) {
-    req.logOut();
-    res.redirect("/");
+	req.logOut();
+	res.redirect("/");
 });
 
 app.get("/lists", function (req, res) {
-    res.redirect("submit");
-    // User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
-    //     if (err) {
-    //         console.log(err);
-    //     } else {
-    //         if (foundUsers) {
-    //             res.render("secrets", { usersWithSecrets: foundUsers })
-    //         }
-    //     }
-    // })
+	res.redirect("submit");
+	// User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
+	//     if (err) {
+	//         console.log(err);
+	//     } else {
+	//         if (foundUsers) {
+	//             res.render("secrets", { usersWithSecrets: foundUsers })
+	//         }
+	//     }
+	// })
 });
 
 app.get("/submit", function (req, res) {
-    if (req.isAuthenticated()) {
-        res.render("submit");
-    } else {
-        res.redirect("/login");
-    }
+	if (req.isAuthenticated()) {
+		res.render("submit", { seriesArray: req.user.series, moviesArray: req.user.movies });
+	} else {
+		res.redirect("/login");
+	}
 });
 
 /* post routes */
 app.post("/submit", function (req, res) {
-    const submittedSecret = req.body.secret;
+	const submittedSecret = req.body.secret;
 
-    /* update database with secret*/
-    User.findById(req.user.id, function (err, foundUser) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                foundUser.secret = submittedSecret;
-                foundUser.save(function () {
-                    // res.redirect("/lists");
-                    res.redirect("/submit");
-                });
-            }
-        }
-    });
+	/* update database with secret*/
+	User.findById(req.user.id, function (err, foundUser) {
+		if (err) {
+			console.log(err);
+		} else {
+			if (foundUser) {
+				foundUser.secret = submittedSecret;
+				foundUser.save(function () {
+					// res.redirect("/lists");
+					res.redirect("/submit");
+				});
+			}
+		}
+	});
 });
 
 app.post("/register", function (req, res) {
-    User.register({ username: req.body.username }, req.body.password, function (
-        err,
-        user
-    ) {
-        if (err) {
-            console.log(err);
-            res.redirect("/register");
-        } else {
-            passport.authenticate("local")(req, res, function () {
-                // res.redirect("/lists"); ///////////////////////
-                res.redirect("/submit");
-            });
-        }
-    });
+	User.register({ username: req.body.username }, req.body.password, function (err, user) {
+		if (err) {
+			console.log(err);
+			res.redirect("/register");
+		} else {
+			passport.authenticate("local")(req, res, function () {
+				// res.redirect("/lists"); ///////////////////////
+				res.redirect("/submit");
+			});
+		}
+	});
 });
 
 app.post("/login", function (req, res) {
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password,
-    });
-    req.login(user, function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            passport.authenticate("local")(req, res, function () {
-                // res.redirect("/lists"); //////////
-                res.redirect("/submit");
-            });
-        }
-    });
+	const user = new User({
+		username: req.body.username,
+		password: req.body.password,
+	});
+	req.login(user, function (err) {
+		if (err) {
+			console.log(err);
+		} else {
+			passport.authenticate("local")(req, res, function () {
+				// res.redirect("/lists"); //////////
+				res.redirect("/submit");
+			});
+		}
+	});
 });
 
 app.post("/submit/tv_series", (req, res) => {
-    const url =
-        "http://www.omdbapi.com/?t=" +
-        req.body.series_name +
-        "&apikey=" +
-        process.env.OMDB_API_KEY;
-    let options = { json: true };
+	const url =
+		"http://www.omdbapi.com/?t=" + req.body.series_name + "&apikey=" + process.env.OMDB_API_KEY;
+	let options = { json: true };
 
-    request(url, options, (error, result, body) => {
-        if (error) {
-            return console.log(error);
-        }
-        if (!error && result.statusCode == 200) {
-            if (body.Response === "True" && body.Type === "series") {
-                User.findById(req.user.id, function (err, foundUser) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        if (foundUser) {
-                            foundUser.series = body;
-                            foundUser.save(function () {
-                                // res.redirect("/lists");
-                                res.redirect("/submit");
-                            });
-                        }
-                    }
-                });
-            } else {
-                res.redirect("/submit");
-            }
-        }
-    });
+	request(url, options, (error, result, body) => {
+		if (error) {
+			return console.log(error);
+		}
+		if (!error && result.statusCode == 200) {
+			if (body.Response === "True" && body.Type === "series") {
+				User.findById(req.user.id, function (err, foundUser) {
+					if (err) {
+						console.log(err);
+					} else {
+						if (foundUser) {
+							foundUser.series.push(body);
+							foundUser.save(function () {
+								res.redirect("/submit");
+							});
+						}
+					}
+				});
+			} else {
+				res.redirect("/submit");
+			}
+		}
+	});
 });
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log("___Server has started sucessfully.");
+	console.log("___Server has started sucessfully.");
 });
